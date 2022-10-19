4dbbc401a8c9c99d4e2fbf89c2ad5bb61c0f97aa
Edoo Junaid <edooj511@gmail.com>
2022-10-19 10:05:43 +0400

    uygyug

--- dafi.zip_expanded/dafi-service-api/src/main/java/group/acensi/dafi/service/api/OrderService.java
+++ dafi.zip_expanded/dafi-service-api/src/main/java/group/acensi/dafi/service/api/OrderService.java
@@ -1,8 +1,6 @@
 package group.acensi.dafi.service.api;
 
-import group.acensi.dafi.entities.Menu;
 import group.acensi.dafi.entities.Order;
-import group.acensi.dafi.service.dto.MenuDto;
 import group.acensi.dafi.service.dto.OrderDto;
 
 public interface OrderService {
