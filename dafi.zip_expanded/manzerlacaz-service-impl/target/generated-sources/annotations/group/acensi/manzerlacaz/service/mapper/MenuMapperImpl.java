05e0a86fad652be8ece0ca8a6a7e0f35dadf541a
Edoo Junaid <edooj511@gmail.com>
2022-10-20 15:45:09 +0400

    likhjilk

--- dafi.zip_expanded/manzerlacaz-service-impl/target/generated-sources/annotations/group/acensi/manzerlacaz/service/mapper/MenuMapperImpl.java
+++ dafi.zip_expanded/manzerlacaz-service-impl/target/generated-sources/annotations/group/acensi/manzerlacaz/service/mapper/MenuMapperImpl.java
@@ -6,7 +6,7 @@ import javax.annotation.processing.Generated;
 
 @Generated(
     value = "org.mapstruct.ap.MappingProcessor",
-    date = "2022-10-20T15:41:42+0400",
+    date = "2022-10-20T15:43:20+0400",
     comments = "version: 1.5.2.Final, compiler: Eclipse JDT (IDE) 1.4.200.v20220802-0458, environment: Java 17.0.4.1 (Eclipse Adoptium)"
 )
 public class MenuMapperImpl implements MenuMapper {
