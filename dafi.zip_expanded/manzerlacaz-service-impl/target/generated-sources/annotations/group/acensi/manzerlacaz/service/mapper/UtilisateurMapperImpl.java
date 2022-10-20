ccd40077e405083a5032823b89fc1de78293cda4
Edoo Junaid <edooj511@gmail.com>
2022-10-20 15:42:29 +0400

    mnbjk

--- dafi.zip_expanded/manzerlacaz-service-impl/target/generated-sources/annotations/group/acensi/manzerlacaz/service/mapper/UtilisateurMapperImpl.java
+++ dafi.zip_expanded/manzerlacaz-service-impl/target/generated-sources/annotations/group/acensi/manzerlacaz/service/mapper/UtilisateurMapperImpl.java
@@ -8,7 +8,7 @@ import javax.annotation.processing.Generated;
 
 @Generated(
     value = "org.mapstruct.ap.MappingProcessor",
-    date = "2022-10-20T15:30:47+0400",
+    date = "2022-10-20T15:41:42+0400",
     comments = "version: 1.5.2.Final, compiler: Eclipse JDT (IDE) 1.4.200.v20220802-0458, environment: Java 17.0.4.1 (Eclipse Adoptium)"
 )
 public class UtilisateurMapperImpl implements UtilisateurMapper {
