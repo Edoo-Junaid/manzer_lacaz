f2bbc8c5a1d55269167190a3ba5fefab28e50b64
Edoo Junaid <edooj511@gmail.com>
2022-10-24 17:20:30 +0400

    removed unused import

--- manzerlacaz/manzerlacaz-service-impl/target/generated-sources/annotations/group/acensi/manzerlacaz/service/mapper/RoleMapperImpl.java
+++ manzerlacaz/manzerlacaz-service-impl/target/generated-sources/annotations/group/acensi/manzerlacaz/service/mapper/RoleMapperImpl.java
@@ -6,7 +6,7 @@ import javax.annotation.processing.Generated;
 
 @Generated(
     value = "org.mapstruct.ap.MappingProcessor",
-    date = "2022-10-24T17:12:45+0400",
+    date = "2022-10-24T17:20:10+0400",
     comments = "version: 1.5.2.Final, compiler: Eclipse JDT (IDE) 1.4.200.v20220802-0458, environment: Java 17.0.4.1 (Eclipse Adoptium)"
 )
 public class RoleMapperImpl implements RoleMapper {
--- manzerlacaz/manzerlacaz-service-impl/target/generated-sources/annotations/group/acensi/manzerlacaz/service/mapper/UtilisateurMapperImpl.java
+++ manzerlacaz/manzerlacaz-service-impl/target/generated-sources/annotations/group/acensi/manzerlacaz/service/mapper/UtilisateurMapperImpl.java
@@ -8,7 +8,7 @@ import javax.annotation.processing.Generated;
 
 @Generated(
     value = "org.mapstruct.ap.MappingProcessor",
-    date = "2022-10-24T17:12:45+0400",
+    date = "2022-10-24T17:19:41+0400",
     comments = "version: 1.5.2.Final, compiler: Eclipse JDT (IDE) 1.4.200.v20220802-0458, environment: Java 17.0.4.1 (Eclipse Adoptium)"
 )
 public class UtilisateurMapperImpl implements UtilisateurMapper {
