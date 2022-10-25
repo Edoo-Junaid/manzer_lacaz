00f856c829278d4bc431cff856d575f134d65ed1
diksha <diksha4@gmail.com>
2022-10-25 14:19:08 +0400

    aaa

--- manzerlacaz/manzerlacaz-service-impl/target/generated-sources/annotations/group/acensi/manzerlacaz/service/mapper/UtilisateurMapperImpl.java
+++ manzerlacaz/manzerlacaz-service-impl/target/generated-sources/annotations/group/acensi/manzerlacaz/service/mapper/UtilisateurMapperImpl.java
@@ -8,7 +8,7 @@ import javax.annotation.processing.Generated;
 
 @Generated(
     value = "org.mapstruct.ap.MappingProcessor",
-    date = "2022-10-25T14:13:25+0400",
+    date = "2022-10-25T14:16:22+0400",
     comments = "version: 1.5.2.Final, compiler: Eclipse JDT (IDE) 1.4.100.v20220318-0906, environment: Java 17.0.3 (Eclipse Adoptium)"
 )
 public class UtilisateurMapperImpl implements UtilisateurMapper {
