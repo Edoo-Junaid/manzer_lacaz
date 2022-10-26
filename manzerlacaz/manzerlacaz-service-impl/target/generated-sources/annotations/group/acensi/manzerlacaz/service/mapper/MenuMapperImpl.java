aa4bc715b97b208c7a48c4aa1983d296b129c538
AliyaToo <aliyahust05@gmail.com>
2022-10-26 11:38:51 +0400

    workkk

--- manzerlacaz/manzerlacaz-service-impl/target/generated-sources/annotations/group/acensi/manzerlacaz/service/mapper/MenuMapperImpl.java
+++ manzerlacaz/manzerlacaz-service-impl/target/generated-sources/annotations/group/acensi/manzerlacaz/service/mapper/MenuMapperImpl.java
@@ -6,8 +6,8 @@ import javax.annotation.processing.Generated;
 
 @Generated(
     value = "org.mapstruct.ap.MappingProcessor",
-    date = "2022-10-26T08:06:23+0400",
-    comments = "version: 1.5.2.Final, compiler: Eclipse JDT (IDE) 1.4.200.v20220802-0458, environment: Java 17.0.4.1 (Eclipse Adoptium)"
+    date = "2022-10-26T11:36:35+0400",
+    comments = "version: 1.5.2.Final, compiler: Eclipse JDT (IDE) 1.4.100.v20220318-0906, environment: Java 17.0.4.1 (Eclipse Adoptium)"
 )
 public class MenuMapperImpl implements MenuMapper {
 
