3e0ea75c7f2760c17ce0ae4904790634358d2848
diksha <diksha4@gmail.com>
2022-10-27 10:18:45 +0400

    hh

--- manzerlacaz/manzerlacaz-service-impl/target/generated-sources/annotations/group/acensi/manzerlacaz/service/mapper/RoleMapperImpl.java
+++ manzerlacaz/manzerlacaz-service-impl/target/generated-sources/annotations/group/acensi/manzerlacaz/service/mapper/RoleMapperImpl.java
@@ -6,8 +6,8 @@ import javax.annotation.processing.Generated;
 
 @Generated(
     value = "org.mapstruct.ap.MappingProcessor",
-    date = "2022-10-27T10:00:44+0400",
-    comments = "version: 1.5.2.Final, compiler: Eclipse JDT (IDE) 1.4.200.v20220802-0458, environment: Java 17.0.4.1 (Eclipse Adoptium)"
+    date = "2022-10-27T10:10:05+0400",
+    comments = "version: 1.5.2.Final, compiler: Eclipse JDT (IDE) 1.4.100.v20220318-0906, environment: Java 17.0.3 (Eclipse Adoptium)"
 )
 public class RoleMapperImpl implements RoleMapper {
 
--- manzerlacaz/manzerlacaz-web/target/generated-sources/annotations/group/acensi/manzerlacaz/web/mapper/UserDetailsMapperImpl.java
+++ manzerlacaz/manzerlacaz-web/target/generated-sources/annotations/group/acensi/manzerlacaz/web/mapper/UserDetailsMapperImpl.java
@@ -4,8 +4,8 @@ import javax.annotation.processing.Generated;
 
 @Generated(
     value = "org.mapstruct.ap.MappingProcessor",
-    date = "2022-10-27T10:00:47+0400",
-    comments = "version: 1.5.2.Final, compiler: Eclipse JDT (IDE) 1.4.200.v20220802-0458, environment: Java 17.0.4.1 (Eclipse Adoptium)"
+    date = "2022-10-27T10:08:37+0400",
+    comments = "version: 1.5.2.Final, compiler: javac, environment: Java 17.0.3 (Eclipse Adoptium)"
 )
 public class UserDetailsMapperImpl implements UserDetailsMapper {
 }
