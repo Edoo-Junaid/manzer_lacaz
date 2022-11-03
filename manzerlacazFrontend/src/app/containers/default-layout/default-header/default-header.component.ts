131594c3f2d69c331d5ebc8d58c3c4ed7ccca715
Edoo Junaid <edooj511@gmail.com>
2022-11-03 15:45:29 +0400

    logout

--- manzerlacazFrontend/src/app/containers/default-layout/default-header/default-header.component.ts
+++ manzerlacazFrontend/src/app/containers/default-layout/default-header/default-header.component.ts
@@ -2,6 +2,8 @@ import {Component, Input, OnInit} from '@angular/core';
 import { FormControl, FormGroup } from '@angular/forms';
 
 import { ClassToggleService, HeaderComponent } from '@coreui/angular';
+import {LoginService} from "../../../services/Login/login.service";
+import {Router} from "@angular/router";
 
 @Component({
   selector: 'app-default-header',
@@ -15,11 +17,17 @@ export class DefaultHeaderComponent extends HeaderComponent implements OnInit{
   public newTasks = new Array(5)
   public newNotifications = new Array(5)
 
-  constructor(private classToggler: ClassToggleService) {
+  constructor(private classToggler: ClassToggleService,private loginService: LoginService, private _router: Router) {
     super();
   }
 
   ngOnInit(): void {
       this.userType=localStorage.getItem('role');
+  }
+
+  Logout($event: MouseEvent) {
+    this.loginService.logOut();
+    this._router.navigateByUrl('login')
+    console.log($event)
   }
 }
